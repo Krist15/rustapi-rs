b95eb30634139caa66a187521344237f788b7f66
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-09-17 23:25:42 -0400

    feat(server): database connection and start server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
-fn main() {
-    println!("Hello, world!");
+mod handler;
+mod model;
+mod schema;
+
+use std::vec;
+
+use actix_cors::Cors;
+use actix_web::middleware::Logger;
+use actix_web::{get, http::header, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use serde_json::json;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
+
+struct AppState {
+    db: Pool<Postgres>,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+    dotenv().ok();
+    env_logger::init();
+
+    let database_url = std::env::var("DATABASE_URL").expect("Database url must be set");
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("Connected to database");
+            pool
+        }
+        Err(err) => {
+            println!("Failed to connect: {:?}", err);
+            std::process::exit(1)
+        }
+    };
+
+    println!("Server started successfully");
+
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:3000")
+            .allowed_methods(vec!["GET", "POST", "PATCH", "DELETE"])
+            .allowed_headers(vec![
+                header::CONTENT_TYPE,
+                header::AUTHORIZATION,
+                header::ACCEPT,
+            ])
+            .supports_credentials();
+
+        App::new()
+            .app_data(web::Data::new(AppState { db: pool.clone() }))
+            .configure(handler::config)
+            .wrap(cors)
+            .wrap(Logger::default())
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
